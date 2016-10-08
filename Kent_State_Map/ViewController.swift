86ee21090176dc92b980c6f73a7f95a0bc57bb01
Omer Al-Madani <Omer-Mac@omers-macbook-pro.local>
2016-10-08 15:18:08 -0400

    Initial Commit

---
+++ Kent_State_Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kent_State_Map
+//
+//  Created by Omer Al-Madani on 10/8/16.
+//  Copyright © 2016 Omer Al-Madani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
